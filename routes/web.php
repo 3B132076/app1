6089526376cefa2ccdcad7c0d6df83f281cf3fdd
許睿舜 <steven921107@yahoo.com.tw>
2025-01-13 16:58:10 +0800

    新增七個路由

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('products', ProductController::class);
+/*
+products.index:     GET|HEAD    /products                   ProductController@index    顯示所有產品的列表
+products.show:      GET|HEAD    /products/{product}         ProductController@show     顯示指定產品的詳細資訊
+products.create:    GET|HEAD    /products/create            ProductController@create   顯示新增產品的表單頁面
+products.store:     POST        /products                   ProductController@store    儲存新增的產品資料
+products.edit:      GET|HEAD    /products/{product}/edit    ProductController@edit     顯示編輯指定產品的表單頁面
+products.update:    PUT|PATCH   /products/{product}         ProductController@update   更新指定產品的資料
+products.destroy:   DELETE      /products/{product}         ProductController@destroy  刪除指定的產品
+*/
