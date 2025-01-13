d1bc0eecedc6462d9699d68a19e27ab9254dc0ea
許睿舜 <steven921107@yahoo.com.tw>
2025-01-13 20:14:01 +0800

    建立各個 model 之間的關係

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
