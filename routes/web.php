a9ca995ee8ca655b12e5ea3642303c760e5cc2cf
許睿舜 <steven921107@yahoo.com.tw>
2025-01-13 17:12:21 +0800

    路由比較

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ Route::get('/', function () {
 });
 Route::resource('products', ProductController::class);
 /*
-products.index:     GET|HEAD    /products                   ProductController@index    顯示所有產品的列表
+products.index:     GET|HEAD    /products                   ProductController@index    顯示所有產品的列表（顯示產品的總覽頁面）
 products.show:      GET|HEAD    /products/{product}         ProductController@show     顯示指定產品的詳細資訊
-products.create:    GET|HEAD    /products/create            ProductController@create   顯示新增產品的表單頁面
+products.create:    GET|HEAD    /products/create            ProductController@create   顯示新增產品的表單頁面（供管理員新增產品）
 products.store:     POST        /products                   ProductController@store    儲存新增的產品資料
-products.edit:      GET|HEAD    /products/{product}/edit    ProductController@edit     顯示編輯指定產品的表單頁面
+products.edit:      GET|HEAD    /products/{product}/edit    ProductController@edit     顯示編輯指定產品的表單頁面（供管理員修改產品資訊）
 products.update:    PUT|PATCH   /products/{product}         ProductController@update   更新指定產品的資料
-products.destroy:   DELETE      /products/{product}         ProductController@destroy  刪除指定的產品
+products.destroy:   DELETE      /products/{product}         ProductController@destroy  刪除指定的產品（從資料庫移除該產品）
 */
